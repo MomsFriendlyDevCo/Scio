207fb3fa1dad8636856fbffb9bfc888ab1fb5c76
Matt Carter <m@ttcarter.com>
2015-08-03 16:31:04 +1000

    Added basic dashboard functionality

---
+++ app/controllers/dashboard.js
@@ -0,0 +1,61 @@
+app.controller('dashboardController', function($scope, $q, $timeout, Servers, Settings) {
+	$scope.loading = true;
+	$scope.loadingSilent = false;
+	$scope.lastRefresh = null;
+	$scope.servers = null;
+	$scope.services = null;
+
+	// Charts {{{
+	$scope.chartData = {
+		// data: [],
+		// keys: [],
+		// labels: [],
+		colors: ['#4285F4', '#FFC107', '#3F51B5', '#00BCD4', '#E91E63', '#607D8B', '#8BC34A', '#673AB7', '#009688'],
+	};
+
+	$scope.donutData = {
+		//data: [],
+	};
+	// }}}
+	
+	// Data refresher {{{
+	$scope.refreshTimer = null;
+	$scope.refresh = function() {
+		$timeout.cancel($scope.refreshTimer);
+		$scope.loadingSilent = true;
+		$q.all([
+			Servers.query({enabled: true}).$promise
+				.then(function(data) {
+					$scope.servers = data
+					// Calculate donut chart data {{{
+					var donutStatus = {
+						ok: {label: 'Ok', value: 0, color: '#4CAF50'},
+						warning: {label: 'Warning', value: 0, color: '#FF9800'},
+						danger: {label: 'Danger', value: 0, color: '#F44336'},
+						error: {label: 'Error', value: 0, color: '#9C27B0'},
+						unknown: {label: 'Unknown', value: 0, color: '#9E9E9E'},
+					};
+					data.forEach(function(server) {
+						if (!donutStatus[server.status]) donutStatus[server.status] = {label: server.status, value: 0};
+						donutStatus[server.status].value++;
+					});
+					$scope.donutData.data = _.values(donutStatus);
+					// }}}
+				}),
+			Servers.chartAll().$promise
+				.then(function(data) {
+					$scope.chartData.data = data.data;
+					$scope.chartData.keys = data.keys;
+					$scope.chartData.labels = data.labels;
+				}),
+		]).finally(function() {
+			$scope.loading = false;
+			$scope.loadingSilent = false;
+			$scope.lastRefresh = moment().format('D/MM/YYYY HH:mm:ss');
+			if (Settings.poll.dashboard)
+				$scope.refreshTimer = $timeout($scope.refresh, Settings.poll.dashboard);
+		});
+	};
+	$scope.refresh();
+	// }}}
+});
--- app/models/settings.js
+++ app/models/settings.js
@@ -1,8 +1,10 @@
 app.factory('Settings', function() {
 	return {
 		poll: {
+			dashboard: 5 * 1000,
 			server: 5 * 1000,
 			servers: 5 * 1000,
+			service: 5 * 1000,
 			services: 5 * 1000,
 		},
 	};
