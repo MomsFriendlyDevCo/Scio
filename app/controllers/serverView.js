4e8fca124b3047e8a385d956d1b391d7083a51b6
Matt Carter <m@ttcarter.com>
2015-08-03 16:09:49 +1000

    Adde donut charts to server view

--- app/controllers/serverView.js
+++ app/controllers/serverView.js
@@ -5,12 +5,18 @@ app.controller('serverViewController', function($scope, $location, $q, $statePar
 	$scope.server = null;
 	$scope.services = null;
 
+	// Charts {{{
 	$scope.chartData = {
 		// data: [],
 		// keys: [],
 		// labels: [],
 		colors: ['#4285F4', '#FFC107', '#3F51B5', '#00BCD4', '#E91E63', '#607D8B', '#8BC34A', '#673AB7', '#009688'],
 	};
+
+	$scope.donutData = {
+		//data: [],
+	};
+	// }}}
 	
 	// Data refresher {{{
 	$scope.refreshTimer = null;
@@ -31,6 +37,21 @@ app.controller('serverViewController', function($scope, $location, $q, $statePar
 			Services.query({server: $stateParams.id}).$promise
 				.then(function(data) {
 					$scope.services = data;
+
+					// Calculate donut chart data {{{
+					var donutStatus = {
+						ok: {label: 'Ok', value: 0, color: '#4CAF50'},
+						warning: {label: 'Warning', value: 0, color: '#FF9800'},
+						danger: {label: 'Danger', value: 0, color: '#F44336'},
+						error: {label: 'Error', value: 0, color: '#9C27B0'},
+						unknown: {label: 'Unknown', value: 0, color: '#9E9E9E'},
+					};
+					data.forEach(function(service) {
+						if (!donutStatus[service.status]) donutStatus[service.status] = {label: service.status, value: 0};
+						donutStatus[service.status].value++;
+					});
+					$scope.donutData.data = _.values(donutStatus);
+					// }}}
 				}),
 		]).finally(function() {
 			$scope.loading = false;
